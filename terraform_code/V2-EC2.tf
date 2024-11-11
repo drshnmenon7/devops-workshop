aa16a7643515fb60854173f7f91d33539ac364c2
Darshan <drshnmenon7@gmail.com>
2024-11-11 11:32:05 +0530

    initial commit of the terraform files

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,36 +1,39 @@
-provider "aws" {
-  region = "us-east-1"
+
+provider aws {
+    region = "ap-south-1"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-    security_groups = [ "demo-sg" ]
+resource "aws_instance" "demo" {
+  ami = "ami-03c68e52484d7488f"
+  instance_type = "t2.micro"
+  key_name = "darshu-devops"
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+ 
 }
 
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "SSH Access"
-  
-  ingress {
-    description      = "Shh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh_access"
+  description = "Allow SSH access inbound and outbound"
+ 
 
   tags = {
-    Name = "ssh-prot"
-
+    Name = "ssh_access"
   }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.ssh_access.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.ssh_access.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
